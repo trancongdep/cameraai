964f0b2cbd071f49002cf72a48032b8fdebec2a6
trancongdep <deptc.gvatvsld@gmail.com>
2026-02-08 08:54:07 +0700

    Update cache name to vdc-safety-v1.31

--- sw.js
+++ sw.js
@@ -1,10 +1,5 @@
-const CACHE_NAME = 'vdc-safety-v1.30';
-const ASSETS = [
-  './index.html',
-  './manifest.json',
-  './ppe.dat',
-  'https://cdn.jsdelivr.net/npm/onnxruntime-web/dist/ort.min.js'
-];
+const CACHE_NAME = 'vdc-safety-v1.31';
+const ASSETS = ['./index.html', './manifest.json', './ppe.dat', 'https://cdn.jsdelivr.net/npm/onnxruntime-web/dist/ort.min.js'];
 self.addEventListener('install', (e) => {
   e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
 });

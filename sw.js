44290281e9ca97e69b1e98595b6b1e4c356d4473
trancongdep <deptc.gvatvsld@gmail.com>
2026-02-08 08:11:45 +0700

    Refactor cache event listeners for clarity

--- sw.js
+++ sw.js
@@ -6,16 +6,10 @@ const ASSETS = [
   'https://cdn.jsdelivr.net/npm/onnxruntime-web/dist/ort.min.js'
 ];
 
-// Cài đặt và lưu trữ vào Cache
 self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
-  );
+  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
 });
 
-// Trả về dữ liệu từ Cache khi không có mạng
 self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then((res) => res || fetch(e.request))
-  );
+  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
 });

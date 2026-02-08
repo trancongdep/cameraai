bf5179afa3ff4334ed051ead043ae4db5b8dc9da
trancongdep <deptc.gvatvsld@gmail.com>
2026-02-08 09:06:21 +0700

    Update CACHE_NAME to version 1.32

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'vdc-safety-v1.31';
+const CACHE_NAME = 'vdc-safety-v1.32';
 const ASSETS = ['./index.html', './manifest.json', './ppe.dat', 'https://cdn.jsdelivr.net/npm/onnxruntime-web/dist/ort.min.js'];
 self.addEventListener('install', (e) => {
   e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));

dd453268c3a3b671bf62d0828f56f37bc79e344f
trancongdep <deptc.gvatvsld@gmail.com>
2026-02-08 08:48:23 +0700

    Remove unnecessary whitespace in sw.js

--- sw.js
+++ sw.js
@@ -5,11 +5,9 @@ const ASSETS = [
   './ppe.dat',
   'https://cdn.jsdelivr.net/npm/onnxruntime-web/dist/ort.min.js'
 ];
-
 self.addEventListener('install', (e) => {
   e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
 });
-
 self.addEventListener('fetch', (e) => {
   e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
 });
